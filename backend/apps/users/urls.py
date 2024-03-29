cf1d3c9017c372fb6252ead115082cff2011b78e
Edwin Niwaha <ed107107@gmail.com>
2024-03-29 19:33:40 +0300

    Change folder structure

--- backend/apps/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import RegisterView, home, profile
-
-urlpatterns = [
-    path("", home, name="users-home"),
-    path("register/", RegisterView.as_view(), name="users-register"),
-    path("profile/", profile, name="users-profile"),
-]
--- backend/static/js/btn_click.js
+++
@@ -1,8 +0,0 @@
-    function validateForm() {
-        var selectedChild = document.getElementById("dropdown").value;
-        if (selectedChild == "") {
-            alert("Please select a child!");
-            return false;
-        }
-        return true;
-    }
