6206a6d262976e0e7ba8fc9aacf09a6d7fb7da9b
Edwin Niwaha <ed107107@gmail.com>
2025-03-22 14:09:05 +0300

    remove magic module

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.models import User
-import magic 
+
 
 from .models import Contact, DocumentUpload, Ebook, Policy, Profile
 
